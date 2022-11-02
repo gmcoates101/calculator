aa97defad591f66cc5d2076c729cca172966d666
Gary Coates <gary.coates@ipfin.co.uk>
2022-11-02 17:06:30 +0000

    Layout and buttons

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.math.exp
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultTextView = findViewById(R.id.resultTextView)
+    }
+
+    fun onCalculate(view: View) {
+        var result = "10"
+
+        resultTextView.text = result
+    }
+
+    fun onClear(view: View) {
+        resultTextView.text = ""
+    }
+
+    fun onDigit(view: View) {
+        val button: Button = view as Button
+
+        resultTextView.append(button.text)
+    }
+
+    fun onOperand(view: View) {
+        val button: Button = view as Button
+        val operand = button.text
+        val lastChar = resultTextView.text[resultTextView.text.length -1]
+        val operands = arrayOf ("/", "*", "-", "+")
+
+        if (!operands.contains<String>(lastChar.toString())) {
+            resultTextView.append("\n${operand}\n")
+        }
+    }
+
+    fun onPoint(view: View) {
+        val point = '.'
+        if (!resultTextView.text.contains(point)) {
+            resultTextView.append(point.toString())
+        }
     }
 }
