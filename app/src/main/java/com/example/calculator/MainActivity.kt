19b3cb289ff6ccffad73978fc5269f9c22bf3289
gmcoates101 <gmcoates101@users.noreply.github.com>
2022-11-04 06:47:27 +0000

    More changes

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,11 +6,12 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import kotlin.math.exp
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var resultTextView: TextView
+    private var point = false
+    private var operator = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,36 +21,45 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onCalculate(view: View) {
-        var result = "10"
+        val result = "10"
+
+        Toast.makeText(this, resultTextView.text, Toast.LENGTH_LONG).show()
 
         resultTextView.text = result
     }
 
     fun onClear(view: View) {
         resultTextView.text = ""
+        point = false
+        operator = true
     }
 
     fun onDigit(view: View) {
         val button: Button = view as Button
 
         resultTextView.append(button.text)
+        operator = false
     }
 
-    fun onOperand(view: View) {
+    fun onOperator(view: View) {
         val button: Button = view as Button
         val operand = button.text
         val lastChar = resultTextView.text[resultTextView.text.length -1]
         val operands = arrayOf ("/", "*", "-", "+")
 
-        if (!operands.contains<String>(lastChar.toString())) {
-            resultTextView.append("\n${operand}\n")
+        val op = (view as Button).text
+
+        if (!operator) {
+            resultTextView.append(" $op\n")
+            point = false
+            operator = true
         }
     }
 
     fun onPoint(view: View) {
-        val point = '.'
-        if (!resultTextView.text.contains(point)) {
-            resultTextView.append(point.toString())
+        if (!point) {
+            resultTextView.append(".")
+            point = true
         }
     }
 }
